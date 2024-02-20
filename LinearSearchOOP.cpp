0d8cf48902e0f766a7f2e34c371c17fc1b162678
gitforvikasrattan <160485566+gitforvikasrattan@users.noreply.github.com>
2024-02-20 10:48:52 +0530

    Fianl version completed

--- LinearSearchOOP.cpp
+++ LinearSearchOOP.cpp
@@ -5,15 +5,28 @@ class search
    int a[60];
    int item;
    int size;
+   int pos;
+   int status; // 0 means not found
 
 
    public:
-       int pos;
-   int status; // 0 means not found
+
+       int retPos()
+       {
+         return pos;
+       }
+
+       int retStatus()
+       {
+           return status;
+       }
+
        void inputArray(int n)
        {
            int i;
            size = n;
+           status = 0;
+           pos = 0;
            for(i=0; i<size; i++)
               cin>>a[i];
        }
@@ -39,10 +52,23 @@ int main()
  {
      search ob1;
      int n;
+     cout<<"Enter the size of array: ";
+     cin>>n;
+     ob1.inputArray(n);
 
-     ob1.inputArray(10);
-     ob1.ls(20);
-     cout<<"Status: " <<ob1.status;
-     cout<<"Pos: " <<ob1.pos;
+     cout<<"\n Enter the Item You want to search: ";
+     int val;
+     cin>>val;
+     ob1.ls(val);
+     if (ob1.retStatus())
+     {
+         cout<<" \n Search Success full\n";
+         cout<<"\nPos: " <<ob1.retPos();
+
+     }
+     else
+        cout<<" \n Search Fail\n";
+
+
 
  }
